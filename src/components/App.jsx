ebfbac3cd5281fc4f4415200c8e4d752dfa47bbe
Stanislav Zabiyaka <stzabiyaka@gmail.com>
2022-06-30 14:46:22 +0300

    folders bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/Transactions/TransactionHistory';
-import { Footer } from './temp/Footer';
+import { Footer } from './Footer/Footer';
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
